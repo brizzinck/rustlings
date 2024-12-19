7555dce060291cb818c262f3d631c92087f27596
brizzinck <87083857+brizzinck@users.noreply.github.com>
2024-12-19 18:20:02 +0200

    solve: 03_if/if2

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,25 +1,23 @@
-// TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
+    } else if ["broccoli", "gummy bears", "literally anything"].contains(&food) {
+        "No thanks!"
     } else {
-        1
+        "else"
     }
 }
 
-fn main() {
-    // You can optionally experiment here.
-}
+fn main() {}
 
-// TODO: Read the tests to understand the desired behavior.
-// Make all tests pass without changing them.
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn yummy_food() {
-        // This means that calling `picky_eater` with the argument "strawberry" should return "Yummy!".
         assert_eq!(picky_eater("strawberry"), "Yummy!");
     }
 
