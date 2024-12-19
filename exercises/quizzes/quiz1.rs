bc574e3604b20183016b16c88bf335598c4d80b8
brizzinck <87083857+brizzinck@users.noreply.github.com>
2024-12-19 18:35:12 +0200

    solve: quizzes/quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -1,22 +1,11 @@
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-// - If
-//
-// Mary is buying apples. The price of an apple is calculated as follows:
-// - An apple costs 2 rustbucks.
-// - However, if Mary buys more than 40 apples, the price of each apple in the
-// entire order is reduced to only 1 rustbuck!
-
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
-
-fn main() {
-    // You can optionally experiment here.
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples > 40 {
+        return apples;
+    }
+    return apples * 2;
 }
+fn main() {}
 
-// Don't change the tests!
 #[cfg(test)]
 mod tests {
     use super::*;
