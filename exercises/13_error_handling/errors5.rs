97e31619120010e6a52bf0e9855bd96441401ee6
brizzinck <87083857+brizzinck@users.noreply.github.com>
2024-12-23 22:05:48 +0200

    solve: 13_error_handling/errors5

--- exercises/13_error_handling/errors5.rs
+++ exercises/13_error_handling/errors5.rs
@@ -20,7 +20,6 @@ enum CreationError {
     Zero,
 }
 
-// This is required so that `CreationError` can implement `Error`.
 impl fmt::Display for CreationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let description = match *self {
@@ -46,9 +45,7 @@ impl PositiveNonzeroInteger {
     }
 }
 
-// TODO: Add the correct return type `Result<(), Box<dyn ???>>`. What can we
-// use to describe both errors? Is there a trait which both errors implement?
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
