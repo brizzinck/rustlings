6843a5d978ea8c864e065639d80f114f3164525a
brizzinck <87083857+brizzinck@users.noreply.github.com>
2024-12-23 22:56:49 +0200

    solve: 17_tests/tests2

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -1,12 +1,8 @@
-// Calculates the power of 2 using a bit shift.
-// `1 << n` is equivalent to "2 to the power of n".
 fn power_of_2(n: u8) -> u64 {
     1 << n
 }
 
-fn main() {
-    // You can optionally experiment here.
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -14,10 +10,9 @@ mod tests {
 
     #[test]
     fn you_can_assert_eq() {
-        // TODO: Test the function `power_of_2` with some values.
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
+        assert_eq!(power_of_2(11), 2048);
+        assert_eq!(power_of_2(13), 8192);
+        assert_eq!(power_of_2(14), 16384);
+        assert_eq!(power_of_2(15), 32768);
     }
 }
