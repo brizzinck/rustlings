1c55c85a8a71dbfd5aa72cbf493767515b96a339
brizzinck <87083857+brizzinck@users.noreply.github.com>
2024-12-25 00:29:12 +0200

    solve: 23_conversions/from_into

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -1,16 +1,9 @@
-// The `From` trait is used for value-to-value conversions. If `From` is
-// implemented, an implementation of `Into` is automatically provided.
-// You can read more about it in the documentation:
-// https://doc.rust-lang.org/std/convert/trait.From.html
-
 #[derive(Debug)]
 struct Person {
     name: String,
     age: u8,
 }
 
-// We implement the Default trait to use it as a fallback when the provided
-// string is not convertible into a `Person` object.
 impl Default for Person {
     fn default() -> Self {
         Self {
@@ -19,30 +12,30 @@ impl Default for Person {
         }
     }
 }
-
-// TODO: Complete this `From` implementation to be able to parse a `Person`
-// out of a string in the form of "Mark,20".
-// Note that you'll need to parse the age component into a `u8` with something
-// like `"4".parse::<u8>()`.
-//
-// Steps:
-// 1. Split the given string on the commas present in it.
-// 2. If the split operation returns less or more than 2 elements, return the
-//    default of `Person`.
-// 3. Use the first element from the split operation as the name.
-// 4. If the name is empty, return the default of `Person`.
-// 5. Parse the second element from the split operation into a `u8` as the age.
-// 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Self::default();
+        }
+        if parts[0].is_empty() {
+            return Self::default();
+        }
+        let age = match parts[1].parse::<u8>() {
+            Ok(age) => age,
+            Err(_) => return Self::default(),
+        };
+        Person {
+            name: String::from(parts[0]),
+            age,
+        }
+    }
 }
 
 fn main() {
-    // Use the `from` function.
     let p1 = Person::from("Mark,20");
     println!("{p1:?}");
 
-    // Since `From` is implemented for Person, we are able to use `Into`.
     let p2: Person = "Gerald,70".into();
     println!("{p2:?}");
 }
